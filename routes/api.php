dcc550a4a67a872d4ce6afe9e166fe2bb8e020f2
TakumaKida <takumakida0@gmail.com>
2026-03-17 02:46:40 +0900

    feat: テスト用のルーティングを追加

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CalcController;
 
+Route::get('/test', function () {
+    return 'Request received';
+});
 Route::post('/calc', [CalcController::class, 'calc']);
